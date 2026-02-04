0930d069592055c4113d4de5e7ee1716d194fb3d
time129600 <time129600@gmail.com>
2026-02-04 15:04:09 +0800

    Fix Unexpected

--- checks/MetaAI.go
+++ checks/MetaAI.go
@@ -7,6 +7,6 @@ import (
 func MetaAI(c http.Client) Result {
 	return CheckGETStatus(c, "https://www.meta.ai/ajax", ResultMap{
 		200: {Status: StatusNo},
-		400: {Status: StatusOK},
+		404: {Status: StatusOK},
 	}, Result{Status: StatusUnexpected})
 }
